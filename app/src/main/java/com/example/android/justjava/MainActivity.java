3db533118b3739cab992f6645cc680cf00c16392
CortezCodes <cortez.mccrary.codes@gmail.com>
2019-09-07 06:09:30 +0300

    Final
    Working Simple Java App for Udacity Course

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,12 +3,69 @@ package com.example.android.justjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        String priceMessage = "Free";
+        displayMessage(priceMessage);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * Change the text on the price of the coffee
+     */
+    private void displayMessage(String message){
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
+    /**
+     * Increments the value of number of coffee up to 3
+     */
+    public void increment(View view){
+        quantity++;
+        display(quantity);
+    }
+
+    /**
+     * Decrement the value of number of coffee to 1
+     */
+    public void decrement(View view){
+        quantity--;
+        display(quantity);
     }
 }
